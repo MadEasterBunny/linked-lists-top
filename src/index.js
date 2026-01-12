49d6a00cdcb60250254cee7ea84e21d3a3690d8d
MadEasterBunny <40764200+MadEasterBunny@users.noreply.github.com>
2026-01-12 23:52:43 +0900

    Add logic for append, prepend and size methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { LinkedList } from "./linked-lists";
 
 const list = new LinkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+console.log(list.size());
--- src/linked-lists.js
+++ src/linked-lists.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor(value, nextNode) {
+    constructor(value, nextNode = null) {
         this.value = value;
         this.nextNode = nextNode;
     }
@@ -7,23 +7,36 @@ class Node {
 
 export class LinkedList {
     constructor() {
-        this.listItem = null;
+        this.headNode = null;
     }
 
     append(value) {
+        if(this.headNode === null) return this.prepend(value);
+        let current = this.headNode;
 
+        while(current.nextNode) {
+            current = current.nextNode;
+        }
+        current.nextNode = new Node(value, null);
     }
 
     prepend(value) {
-
+        const node = new Node(value, this.headNode);
+        this.headNode = node;
     }
 
     size() {
-        
+        let count = 0;
+        let current = this.headNode;
+        while(current !== null) {
+            count++;
+            current = current.nextNode;
+        }
+        return count;
     }
 
     head() {
-
+        
     }
 
     tail() {
